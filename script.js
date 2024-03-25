acfb8a41fee48e513c4abd29d508384aca47c67d
Julio Cesar <juliocesaromartins@gmail.com>
2024-03-25 20:24:57 -0300

    nono commit do css e terceiro commit do js

--- script.js
+++ script.js
@@ -31,6 +31,13 @@ botoesGorjeta.forEach(botao => {
     botao.addEventListener("click", receberPorcentagem)
 })
 
-function receberPorcentagem(evento) {
-    console.log(evento.target.value)
+function receberPorcentagemBotao(evento) { 
+    botoesGorjeta.forEach(botao => {
+        botao.classList.remove("botao-ativo")
+
+        if(botao.value === evento.target.value)
+            botao.classList.add("botao.ativo")
+    })
+
+    porcentagem = parseFloat(evento.target.value) / 100
 }
