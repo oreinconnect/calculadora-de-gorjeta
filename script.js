1912b7a5133d39a46e33d26e649c451bbb284f8c
Julio Cesar <juliocesaromartins@gmail.com>
2024-03-25 20:30:39 -0300

    quarto commit do js

--- script.js
+++ script.js
@@ -31,13 +31,23 @@ botoesGorjeta.forEach(botao => {
     botao.addEventListener("click", receberPorcentagem)
 })
 
-function receberPorcentagemBotao(evento) { 
+function receberPorcentagem(evento) { 
     botoesGorjeta.forEach(botao => {
         botao.classList.remove("botao-ativo")
 
-        if(botao.value === evento.target.value)
+        if(botao.value === evento.target.value) {
             botao.classList.add("botao.ativo")
+        }
     })
 
-    porcentagem = parseFloat(evento.target.value) / 100
+    if(evento.target.value !== ""){ 
+        porcentagem = parseFloat(evento.target.value) / 100
+    } else{
+        porcentagem = 0
+    }
+
+    console.log(porcentagem)
 }
+
+const gorjetaInput = document.querySelector("#outra")
+gorjetaInput.addEventListener("input", receberPorcentagem)
